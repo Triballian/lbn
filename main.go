33b5558d8e6d30e2d16eb0ef75e121e92421042f
Triballian <candiapps@gmail.com>
2016-09-20 17:37:13 -0500

    added FlashCard struct and FlashCards struct

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+type FlashCard struct {
+	Simplified string
+	English string
+	Dictionary *dictionary.Dictionary
+}
+
+type FlashCards struct {
+	Name string
+	CardOrder string
+	ShowHalf string
+	Cards []*FlashCards
+}
 func main() {
 
 }
